4142834591927613cbcc56d28598be43187686bc
mchlol <m.andreabr@gmail.com>
2021-06-24 18:58:57 +1000

    add existing files

---
+++ script.js
@@ -0,0 +1,6 @@
+let box = document.querySelector('#recipe-box');
+
+/* when a recipe title is clicked, a BOX below the list of recipes should appear with the RECIPE inside */
+
+let recipeTitle = "";
+let recipeContent = "";
