6ca6793f663fe493d62204ff8f06e1b562d73544
mchlol <m.andreabr@gmail.com>
2021-09-05 16:13:57 +1000

    created display function

--- script.js
+++ script.js
@@ -1,11 +1,29 @@
-const recipeContainer = document.querySelector('#recipe-container');
+// TEMPLATE
+/*
+const recipe = {
+  title: "",
+  image: ),
+  prepTime: "",
+  serves: 4,
+  sourceLink: ""),
+  sourceName: "",
+  ingredientsList: [
 
-const sausageRecipe = {
+  ],
+  instructions: [
+
+  ],
+};
+*/
+
+// RECIPE OBJECTS
+
+const sausage = {
   title: "Smoky Sausage Hotpot",
-  image: URL(assets/smoky-sasuage-hotpot.jpeg),
+  image: "./assets/smoky-sasuage-hotpot.jpeg",
   prepTime: "1hr",
   serves: 4,
-  sourceLink: URL('https://blog.goodpairdays.com/smoky-sausage-hotpot/'),
+  sourceLink: 'https://blog.goodpairdays.com/smoky-sausage-hotpot/',
   sourceName: "Good Pair Days",
   ingredientsList: [
     "1 tsbp olive oil", 
@@ -23,7 +41,7 @@ const sausageRecipe = {
     "1 tsp fresh thyme, to serve",
     "Crusty bread, to serve",
   ],
-  method: [
+  instructions: [
     "Chop the onion. Crush the garlic. Chop the celery. Chop the capsicum.",
     "Heat the oil over a medium heat on the stove in an ovenproof dish. Add the onion and cook for 5 minutes.",
     "Add the garlic, capsicum, and celery, and cook for another 5 minutes.",
@@ -36,3 +54,215 @@ const sausageRecipe = {
 };
 
 
+const chilli = {
+  title: "Caribbean Beef Chilli",
+  image: "./assets/caribbean-beef-chilli-coconut-rice.jpeg",
+  prepTime: "35mins",
+  serves: 2,
+  sourceLink: "https://www.hellofresh.com.au/recipes/caribbean-beef-chilli-coconut-rice-596d8638a2882a678d0c6a42",
+  sourceName: "HelloFresh",
+  ingredientsList: [
+    "1 carrot (unpeeled)",
+    "2 cloves of garlic",
+    "1 cup of basmati rice",
+    "2 cups of water",
+    "2 tbsp Jerk seasoning",
+    "500g beef mince",
+    "50g mango chutney",
+    "2 tbsp tomato paste",
+    "1 pinch of chilli flakes (optional)",
+    "1 cucumber",
+    "1 roma tomato",
+    "1 bunch of mint",
+    "1/2 lemon",
+  ],
+  instructions: [
+    "Grate the carrot (unpeeled). Peel and crush the garlic. Slice the lemon into wedges, and juice until you have 2 tsp.",
+    "While the rice is cooking, heat a drizzle of olive oil in a large frying pan over a medium-high heat. Add the beef mince and cook, breaking up with a wooden spoon, for 4-5 minutes, or until browned. Add the carrot, garlic, and jerk spice blend and cook for 1-2 minutes, or until fragrant.",
+    "Add the mango chutney, tomato paste, 1 cup of water, 1 tsp of salt, and the chilli flakes (if using) to the pan with the beef mince. Mix well and simmer for 2 minutes, or until thickened.",
+    "While the chilli is simmering, dice the cucumber and the Roma tomato. Roughly chop the mint leaves. In a medium bowl, combine the cucumber, tomato, mint, lemon juice and a drizzle of olive oil. Season to taste with a pinch of salt and pepper and stir to combine.",
+    "Divide the coconut rice between bowls and top with the Caribbean beef chilli. Spoon over the cucumber-mint salsa.",
+  ],
+};
+
+const lentils = {
+  title: "Smoky Chicken & Lentils",
+  image: "./assets/smoky-chicken-lentils.jpeg",
+  prepTime: "40mins",
+  serves: 4,
+  sourceLink: "https://www.lentils.org/recipe/moroccan-braised-chicken-lentils-smoked-paprika-tomato/",
+  sourceName: "Lentils.org",
+  ingredientsList: [
+    "1 chicken cut into 8 pieces or equivalent in breasts and/or thighs, skin on.",
+    "1 medium brown onion, finely chopped",
+    "2 cloves of garlic, thinly sliced",
+    "1.5 tbsp smoked paprika",
+    "1 tbsp cumin",
+    "1 cup split red lentils, rinsed",
+    "1 cup whole cherry tomatoes",
+    "2 cups/500mL chicken stock",
+    "3 bay leaves",
+    "0.5 tsp chilli flakes",
+    "Parsley to garnish, finely chopped",
+  ],
+  instructions: [
+    "Preheat the oven to 200 celsius. Finely chop the onion. Slice the garlic. Rinse the lentils. Make the chicken stock.",
+    "Pat both sides of chicken with paper towel to remove excess moisture, season generously on both sides with salt and pepper.",
+    "In an ovenproof fry pan or cast iron pan, heat a splash of olive oil over a medium-high heat. Add the chicken to the pan and cook for 4-5 mins each side. Sear the chicken in batches or they will steam and not sear. Remove from the pan and set aside.",
+    "Turn heat to medium, add onions, garlic, 1.5 tbsp smoked paprika, and 1 tbsp cumin. Stir for 3-5 mins. You won't be able to see it but the onions should go translucent but not browned.",
+    "Turn off the heat. Add lentils, stock, tomatoes, bay leaves, chilli flakes, and chicken to the pan.",
+    "Slide the pan into the oven and cook uncovered for 20-25 mins.",
+    "Turn off oven and turn on broiler (grill) for 1-2 mins. This is the brown up the tomatoes. Remove from the oven & serve garnished with chopped parsley.",
+  ],
+};
+
+const tomato = {
+  title: "Chicken & Tomato with Capers",
+  image: "./assets/chicken-tomato-capers.png",
+  prepTime: "30mins",
+  serves: 4,
+  sourceLink: "https://diethood.com/chicken-tomato-sauce/",
+  sourceName: "Diethood",
+  ingredientsList: [
+    "2 tbsp olive oil",
+    "4 boneless chicken breasts",
+    "1 tsp dried basil",
+    "salt and fresh ground pepper to taste",
+    "1 tbsp butter",
+    "1 brown onion finely diced",
+    "2 tsp minced garlic",
+    "1/3 cup white wine vinegar",
+    "1 400g can of diced tomatoes",
+    "2 tbsp tomato paste",
+    "1 tsp dried oregano",
+    "another 1 tsp dried basil",
+    "1/3 cup cream",
+    "1/4 cup drained capers",
+    "1/4 cup chopped fresh parsley for garnish",
+  ],
+  instructions: [
+    "Heat olive oil in a large skillet.",
+    "Season chicken with dried basil, salt, and pepper.",
+    "Add the chicken breasts to the skillet and cook over medium-high heat, 3 to 4 minutes per side, or until lightly browned.",
+    "Remove chicken breasts from skillet; set aside.",
+    "  Add butter to the skillet and melt.",
+    "Add diced onions and cook for 2 minutes.",
+    "Stir in garlic and cook for 30 seconds.",
+    "Stir in the wine to dissolve the brown pieces found at the bottom of the skillet; continue to cook for 4 minutes, or until reduced.",
+    "Mix in the tomatoes, tomato paste, oregano, basil, half & half, and capers.",
+    "Place chicken back in the skillet, bring to a boil; cover and simmer over medium-low heat for 8 minutes, or until chicken is cooked through.",
+    "Remove from heat, garnish with parsley and serve.",
+  ],
+};
+
+const salmon = {
+  title: "Parmesan Crusted Salmon",
+  image: "./assets/parmesan-crusted-salmon.png",
+  prepTime: "30mins",
+  serves: 2,
+  sourceLink: "https://tasty.co/recipe/parmesan-crusted-salmon",
+  sourceName: "Tasty.co",
+  ingredientsList: [
+    "4 pieces of salmon, skin off",
+    "2 bunches of asparagus",
+    "1 egg",
+    "1/4 cup panko breadcrumbs",
+    "1/4 cup grated parmesan cheese",
+    "1 tbsp chopped parsley",
+    "salt, pepper, olive oil",
+  ],
+  instructions: [
+    "Grate the cheese, chop the parsley",
+    "Preheat the oven to 200 celsius",
+    "In a medium bowl, combine the breadcrumbs, parmesan, parsley, and salt to taste.",
+    "In a separate bowl, whisk the egg. Dredge the salmon in the egg, then dip into the breadcrumb mix & cover completely.",
+    "Lay the salmon on a baking sheet. Lay asparagus beside the salmon & drizzle with olive oil and season with salt & pepper.",
+    "Bake for 10-12 minutes.",
+    "Serve.",
+  ],
+};
+
+
+/*
+recipe divs add event listener 'click'
+runs function to append button.id recipe to container
+*/
+
+const links = document.querySelectorAll(".recipe-link");
+console.log(links);
+
+links.forEach((link => {
+  link.addEventListener('click', () => {
+    // run function to show recipe based on id
+    let byTitle = link.id; // DRY: check for something to match to so the switch statement can look for true instead of a specified id e.g if true return object with that title
+    switch (byTitle) {
+      case "chilli":
+        link.style.backgroundColor = "rebeccaPurple";
+        showRecipe(chilli);
+        break;
+      case "lentils":
+        link.style.backgroundColor = "red";
+        showRecipe(lentils);
+        break;
+      case "tomato":
+        link.style.backgroundColor = "orange";
+        showRecipe(tomato);
+        break;
+      case "curry":
+        link.style.backgroundColor = "green";
+        showRecipe(curry);
+        break;
+      case "salmon":
+        link.style.backgroundColor = "pink";
+        showRecipe(salmon);
+        break;
+      case "sausage":
+        link.style.backgroundColor = "lightBlue";
+        showRecipe(sausage);
+        break;
+      default:
+        link.style.backgroundColor = "grey";
+    }
+  })
+}));
+
+// select the section to hold recipe divs
+const recipeContainer = document.querySelector('#recipe-container');
+
+// CREATE THE FUNCTION
+// the function needs to take in an object (recipe), and display all of the information associated with that object in html.
+
+function showRecipe(recipe) {
+  
+  let displayIngredients = document.createElement('ul');
+  for (let i = 0; i < recipe.ingredientsList.length; i++) {
+    let ingredientListItem = document.createElement('li');
+    ingredient = recipe.ingredientsList[i];
+    console.log(ingredient);
+    ingredientListItem.textContent = ingredient;
+    displayIngredients.appendChild(ingredientListItem);
+  }
+
+  // same for instructions
+
+  let displayInstructions = document.createElement('ol');
+
+  let displayHeadings = `
+  <div id="recipe-details">
+    <h2>${recipe.title}</h2>
+    <img class="recipe-img" src="${recipe.image}" />
+    <p>Prep Time: ${recipe.prepTime}. Serves: ${recipe.serves}. Source: <a href="${recipe.sourceLink}" target="_blank">${recipe.sourceName}</a></p>
+    <h3>Ingredients</h3>
+    ${displayIngredients}
+    <h3>Instructions</h3>
+    ${displayInstructions}
+    `;
+
+  const recipeDetails = document.createElement("div");
+  recipeDetails.id = "recipe-details";
+  recipeDetails.innerHTML = displayHeadings;
+  recipeContainer.appendChild(recipeDetails);
+
+};
+
+// need to be able to replace the currently showing recipe with the next one selected
